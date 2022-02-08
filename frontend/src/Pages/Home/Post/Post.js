5d578b679949c7c151692ab664ef1eddd396ffd2
ruibing <rbruibingnet@gmail.com>
2022-02-08 09:59:26 +0800

    css for posts on homepage

--- frontend/src/Pages/Home/Post/Post.js
+++ frontend/src/Pages/Home/Post/Post.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import styles from "./Post.module.css";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import InsertCommentRoundedIcon from "@mui/icons-material/InsertCommentRounded";
+import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
+import { IconButton } from "@mui/material";
 
-const Post = ({timestamp, content, user_name}) => {
+const Post = ({ timestamp, content, name, surname, likes }) => {
   return (
-    <div>
-      <div>{user_name}</div>
-      <div>{content}</div>
-      <div>{(new Date(timestamp)).toLocaleString()}</div>
+    <div className="card" id={styles.container}>
+      <div id={styles.innerLeftContainer}>
+        <div id={styles.user}>
+          <AccountCircleRoundedIcon sx={{ fontSize: 40, color: "#28359390" }} />
+          <div className="title" id={styles.name}>{`${name} ${surname}`}</div>
+        </div>
+        <div>{content}</div>
+        <div id={styles.timestamp}>{new Date(timestamp).toLocaleString()}</div>
+      </div>
+      <div id={styles.innerRightContainer}>
+        <div>{likes}</div>
+        <div>
+          <IconButton>
+            <FavoriteRoundedIcon />
+          </IconButton>
+        </div>
+        <div>
+          <IconButton>
+            <InsertCommentRoundedIcon />
+          </IconButton>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
