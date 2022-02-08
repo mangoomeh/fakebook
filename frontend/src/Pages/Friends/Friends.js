8feb2aececb165537573c0bf27d11e8765770a15
ruibing <rbruibingnet@gmail.com>
2022-02-08 09:59:58 +0800

    set up css and friend page

--- frontend/src/Pages/Friends/Friends.js
+++ frontend/src/Pages/Friends/Friends.js
@@ -1,31 +1,35 @@
 import React, { useContext, useEffect, useState } from "react";
 import fetcher from "../../Auth/Axios";
+import PeopleCard from "../../Components/PeopleCard/PeopleCard";
 import UserContext from "../../Context/UserContext";
 
 const Friends = () => {
   const { accessToken, refreshToken, setAccessToken } = useContext(UserContext);
   const [friends, setFriends] = useState([]);
 
+  const fetchFriends = async () => {
+    const verifiedToken = await fetcher.verifyAndRefresh(
+      accessToken,
+      refreshToken
+    );
+    const data = await fetcher.get("api/friends/", verifiedToken);
+    setFriends(data);
+    if (accessToken !== verifiedToken) {
+      setAccessToken(verifiedToken);
+    }
+  };
+
   useEffect(() => {
-    const fetchFriends = async () => {
-      const verifiedToken = await fetcher.verifyAndRefresh(
-        accessToken,
-        refreshToken
-      );
-      const data = await fetcher.get("api/friends/", verifiedToken);
-      setFriends(data);
-      if (accessToken !== verifiedToken) {
-        setAccessToken(verifiedToken);
-      }
-    };
     fetchFriends();
   }, []);
+
   return (
     <div className="page">
       <h1>Friends</h1>
       <div>
         {friends.map((friend) => {
-          return <div>{`${friend.name} ${friend.surname}`}</div>;
+          console.log(friend)
+          return <PeopleCard {...friend} status="friend" dataFetcher={fetchFriends} />;
         })}
       </div>
     </div>
