d54acabbf8c26c394c69fbc36fcab551475e8cda
ruibing <rbruibingnet@gmail.com>
2022-02-13 16:12:31 +0800

    change from using get_user_model() to settings.AUTH_USER_MODEL for models.py in restful_apis

--- backend/restful_apis/models.py
+++ backend/restful_apis/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.conf import settings
 
 
 class Post(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(
-        get_user_model(), related_name="post_liker", blank=True)
+        settings.AUTH_USER_MODEL, related_name="post_liker", blank=True)
 
     class Meta:
         ordering = ['-timestamp']
@@ -15,19 +16,19 @@ class Post(models.Model):
 
 class FriendRequest(models.Model):
     requester = models.ForeignKey(
-        get_user_model(), related_name="requester_name", on_delete=models.CASCADE)
+        settings.AUTH_USER_MODEL, related_name="requester_name", on_delete=models.CASCADE)
     accepter = models.ForeignKey(
-        get_user_model(), related_name="accepter_name", on_delete=models.CASCADE)
+        settings.AUTH_USER_MODEL, related_name="accepter_name", on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
     user = models.ForeignKey(
-        get_user_model(), related_name="commenter", on_delete=models.CASCADE)
+        settings.AUTH_USER_MODEL, related_name="commenter", on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(
-        get_user_model(), related_name="comment_liker", blank=True
+        settings.AUTH_USER_MODEL, related_name="comment_liker", blank=True
     )
 
     class Meta:
